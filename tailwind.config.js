fb6038370f114c49107ac3ff5c3253b96d6a0557
Utsav Mahyawanshi <utsavgmahyawanshi@gmail.com>
2025-08-14 09:14:15 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // This ensures Tailwind scans all component files
+  ],
+  theme: {
+    extend: {}, // You can customize colors, fonts, etc. here
+  },
+  plugins: [],
+}
